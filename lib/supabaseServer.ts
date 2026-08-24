50f6b232c36e7f2868f3dc326686378e625a8ba3
joshual823 <joshuailouis@gmail.com>
2026-08-24 16:40:09 -0400

    Update supabaseServer.ts
    fix build error

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -12,10 +12,10 @@ export function createClient() {
         get(name: string) {
           return cookieStore.get(name)?.value
         },
-        set(name: string, value: string, options) {
+        set(name: string, value: string, options: any) {
           cookieStore.set({ name, value, ...options })
         },
-        remove(name: string, options) {
+        remove(name: string, options: any) {
           cookieStore.set({ name, value: '', ...options })
         },
       },
